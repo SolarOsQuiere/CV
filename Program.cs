a28375d4c6705ecd9b88ad13467619d53c778dc4
hesolar <hectorsolar99@gmail.com>
2026-01-08 16:37:17 +0100

    FUN ' Refactor enrutador y limpieza de componentes raíz

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<Inicio>("#preRenderComponent");
+//builder.RootComponents.Add<Inicio>("#preRenderComponent");
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
